32141bc654e0d411e576036d028ee0fcb4be6760
AliasNoName <mirko@dump.hr>
2016-02-21 19:52:18 +0100

    Removed project, starting clean solution

--- NWT.Twitter/Models/Data/FavouriteTweet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NWT.Twitter.Models.Data
-{
-    public class FavouriteTweet
-    {
-        public int Id { get; set; }
-        public Tweet Tweet { get; set; }
-        public User User { get; set; }
-    }
-}
